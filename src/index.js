c2d64115d521ad47b794bee3250d43e49cb2d75b
josephferrairo <josephferrairo@gmail.com>
2016-04-20 23:31:15 -0400

    built list with map

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCHVZnkIYYz8lDb19-AXlL4ULED2W94KV0';
 
 
@@ -21,6 +22,7 @@ class App extends Component {
   render () {
     return (<div>
       <SearchBar />
+      <VideoList videos={this.state.videos} />
     </div>
   );
 }
