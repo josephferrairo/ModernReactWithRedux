961b7a9d36b01cc95de817c6b85d0c1379a1b83e
josephferrairo <josephferrairo@gmail.com>
2016-04-20 23:41:20 -0400

    added YT keys to list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,7 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} />
+    return <VideoListItem key={video.etag} video={video} />
   });
 
   return (
@@ -12,6 +12,6 @@ const VideoList = (props) => {
       {videoItems}
     </ul>
   );
-}
+};
 
 export default VideoList;
